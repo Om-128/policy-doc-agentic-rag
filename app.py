22544db291fadff9a1da3ab018a2c7456a3ec8b0
Om Tambat <omtambat284@gmail.com>
2026-01-27 19:35:08 +0530

    add docker build CI workflow

--- app.py
+++ app.py
@@ -59,6 +59,7 @@ def stream():
         )
     except Exception as e:
         raise CustomException(e, sys)
-        
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
